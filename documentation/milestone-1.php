2355738634b8f90d2e3b5b49ed8f9ba303eb0db3
Marcelo Ibarra <zulymar.mi@gmail.com>
2016-07-20 10:36:20 -0600

    started process on personal website

---
+++ documentation/milestone-1.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>Marcelo's Personal Website</title>
+	</head>
+	<body>
+		<header>
+			<h1>Ebay Data Design</h1>
+			<h2>User Story</h2>
+			<p>as a user, looking for a pair of brown shoes</p>
+			<h2>Persona</h2>
+			<p>Mark - likes this site where he can do on-line shopping easy and hassle free. Being able to browse
+				through different products can help him save lots of time and avoid crowds.
+		</header>
---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="eng">
+	<head>Marcelo Ibarra - Personal Website</head>
+	<body>
+		<header>Purpose: Future Developer who wants to get acquainted with potential<br>employers in the programming field who may want use my services</header>
+		<main>
+		</main>
+	</body>
+	<footer>
+	</footer>
+</html>
