b74dbc34836094895ccbacf5143e3f2c79ea342f
Marcelo Ibarra <zulymar.mi@gmail.com>
2016-07-20 21:47:20 -0600

    completed the personal website project - milestone 1

--- documentation/milestone-1.php
+++ documentation/milestone-1.php
@@ -5,10 +5,24 @@
 	</head>
 	<body>
 		<header>
-			<h1>Ebay Data Design</h1>
-			<h2>User Story</h2>
-			<p>as a user, looking for a pair of brown shoes</p>
-			<h2>Persona</h2>
-			<p>Mark - likes this site where he can do on-line shopping easy and hassle free. Being able to browse
-				through different products can help him save lots of time and avoid crowds.
+			<h3>Purpose</h3>
+			<p>I'm a dedicated/hardworking future web develorer who likes challenge and enjoys learning new things.<br>I'd
+				like to get acquainted to potential employers and collegues.</p>
+			<h3>Audience</h3>
+			<p>This is intended to companies and collegues that may know of any employment opportunities <br> as a web developer
+				who may need freelance work or maybe able to offer a full-time position or an intership.</p>
+			<h3>Goal</h3>
+			<p>I'll like to acquire full time work for any company or to be able to do any freelance work for potential clients or friends.</p>
+			<h3>Persona</h3>
+			<p><strong>Name: </strong>Abel P. Garza</p>
+			<p><strong>Age: </strong>39</p>
+			<p><strong>Profession: </strong>Founder of DSFederal who's looking to hire a full-time, in-house Front-End Web Developer. Candidate should have exceptional communication and organizational skills to provide website development support and maintenance.</p>
+				<p><strong>Technology: </strong>Use a HP computer at home, a MacBook Pro laptop on a regular basis, and an Android HT7 with an unlimited 4G data connection.
+			<p><strong>Attitudes and Behaviors: </strong>Although always on a tight schedule; always doing everything possible to go the extra mile to get the right candidate on board as soon as possible. Very open minded and involved with the community by attending as many networking events as possible.</p>
+			<p><strong>Frustrations and Needs: </strong>Abel needs to hire someone knowledgeable as soon as possible with a negotiable salary and great benefits included. Abel considers a great fit for this position; someone who has great personality and can be a great team player. He expects to have this position filled in the next week.</p>
+			<p><strong>Goals: </strong>Abel plans on not spending too much time reviewing every resume. He will concentrate on making the right decision as soon as possible by the end of this week</p>
+			<h3>Use Case</h3>
+			<p>Recently; I've submitted a cover letter and a resume to Abel P. Garza for this available Entry-Level Web Developer position. He told me that he was going to consider my resume in his next batch to review in the next couple of days and was going to have an answer for me by Thursday afternoon via e-mail<br> He also; stated that he was even going to make time to review my webpage tomorrow after I handed him one of my personalized business cards to review more information about me <br> Aside from leting me know that he had eleven more candidates to consider; He also advised me that he was going to make a final decision by Friday after his meeting with the hiring committee.</p>
 		</header>
+		</body>
+		</html>
