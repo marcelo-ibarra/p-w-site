3e2ace824b27c92dda12be16df9c54d33f00af00
Marcelo Ibarra <zulymar.mi@gmail.com>
2016-07-25 14:14:16 -0600

    linked css to html file

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -1,6 +1,8 @@
 <!DOCTYPE html>
 <html lang="en">
 	<head>
+		<meta charset="utf-8"/>
+		<link href="css/style.css" rel="stylesheet" type="text/css"/>
 		<title>Marcelo's Personal Website</title>
 	</head>
 	<body>
---
+++ public_html/documentation/milestone-2.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: STEMulus
+ * Date: 7/22/2016
+ * Time: 8:07 AM
+ */
