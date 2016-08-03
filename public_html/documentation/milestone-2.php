7ce4ca8e6dbd45bf016a18db87d1cb549cb7fbfa
Marcelo Ibarra <zulymar.mi@gmail.com>
2016-08-03 13:07:08 -0600

    Completed my feedback for p-w-site; will make other corrections if needed

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -1,7 +1,33 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: STEMulus
- * Date: 7/22/2016
- * Time: 8:07 AM
- */
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset ="utf-8" />
+		<title>milestone-2</title>
+	</head>
+	<body>
+		<header>
+			<h1>Professional Portfolio</h1>
+		</header>
+		<h2>Site Map</h2>
+		<ul>
+			<li>About</li>
+			<p>This page will display information about me. This will let potential customers(0rganizations) about the type of work I have done and what kind of experience I've acquired. This will be helpful for them because they'll have access to all my information. They'll notice that I'm looking for employment opportunities (Intership) only for the companies where I could be a great fit.</p>
+			<li>Contact</li>
+			<p>This page will contain additional information about me such as: links to my professional social media profiles: LinkedIn, Twitter, Facebook, GitHub, etc.</p>
+			<li>Gallery</li>
+			<p>This area will display a couple of pictures about my theme page. It could be open to display any other type of photos that I may want to display here.</p>
+			<li>Home</li>
+			<p>An introductory page that can show that I'm a recently graduated Junior Developer and can take the user to the different available tabs on my page.</p>
+			<li>Portfolio</li>
+			<p>This will display a few pieces of my most recent work that I want people to be able to see.</p>
+			<h2>Goal</h2>
+		</ul>
+		<p>My goal is to be able to layout a page with a very nice header that it's very user friendly. The navigation would be linked to the different parts of the site so I'll be able to navigate through it easily to different areas and search for whatever I want at that moment.</p>
+		<h2>Mobile wireframe</h2>
+		<img src="../../img/mobile.png" alt="mobile.png" />
+		<h2>Desktop wireframe</h2>
+		<img src="../../img/desktop.png" alt="desktop.png" />
+		<h2>Site Mapping</h2>
+		<img src="../../img/mapping.png" alt="mapping.png" />
+	</body>
+</html>
